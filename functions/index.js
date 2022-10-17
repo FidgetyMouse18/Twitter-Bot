7fb61ef69afcbe121c946a4ec88e2a3c1d834595
FidgetyMouse18 <redflare16@gmail.com>
2022-10-17 16:44:21 +1000

    Initial Commit

---
+++ functions/index.js
@@ -0,0 +1,148 @@
+const TwitterApi = require('twitter-api-v2').default;
+const functions = require("firebase-functions");
+const admin = require('firebase-admin')
+const getMeme = require("./scrape");
+const axios = require('axios');
+const config = require("./config")
+admin.initializeApp();
+
+const dbRefMeme = admin.firestore().doc('tokens/meme')
+const dbRefQuote = admin.firestore().doc('tokens/quote')
+
+exports.authMeme = functions.region('australia-southeast1').runWith({ timeoutSeconds: 60 }).https.onRequest(async (request, response) => {
+    const client = new TwitterApi({
+        appKey: config.memeConfig.appKey,
+        appSecret: config.memeConfig.appSecret
+    })
+    const auth = await client.generateAuthLink(
+        config.callbackURLMeme,
+        { linkMode: 'authorize' }
+    );
+
+    // store verifier
+    await dbRefMeme.set({ token: auth.oauth_token, secretToken: auth.oauth_token_secret });
+
+    response.redirect(auth.url);
+});
+
+exports.authQuote = functions.region('australia-southeast1').https.onRequest(async (request, response) => {
+    const client = new TwitterApi({
+        appKey: config.quoteConfig.appKey,
+        appSecret: config.quoteConfig.appSecret
+    })
+    const auth = await client.generateAuthLink(
+        config.callbackURLQuote,
+        { linkMode: 'authorize' }
+    );
+
+    // store verifier
+    await dbRefQuote.set({ token: auth.oauth_token, secretToken: auth.oauth_token_secret });
+
+    response.redirect(auth.url);
+});
+
+exports.callbackMeme = functions.region('australia-southeast1').https.onRequest(async (request, response) => {
+    const { oauth_token, oauth_verifier } = request.query;
+
+    const dbSnapshot = await dbRefMeme.get();
+    const { secretToken } = dbSnapshot.data();
+
+    if (!oauth_token || !oauth_verifier || !secretToken) {
+        return res.status(400).send('You denied the app or your session expired!');
+    }
+
+    const client = new TwitterApi({
+        appKey: config.memeConfig.appKey,
+        appSecret: config.memeConfig.appSecret,
+        accessToken: oauth_token,
+        accessSecret: secretToken,
+    });
+    const result = await client.login(oauth_verifier);
+
+    await dbRefMeme.set({ token: result.accessToken, secretToken: result.accessSecret });
+
+});
+
+exports.callbackQuote = functions.region('australia-southeast1').https.onRequest(async (request, response) => {
+    const { oauth_token, oauth_verifier } = request.query;
+
+    const dbSnapshot = await dbRefQuote.get();
+    const { secretToken } = dbSnapshot.data();
+
+    if (!oauth_token || !oauth_verifier || !secretToken) {
+        return res.status(400).send('You denied the app or your session expired!');
+    }
+
+    const client = new TwitterApi({
+        appKey: config.quoteConfig.appKey,
+        appSecret: config.quoteConfig.appSecret,
+        accessToken: oauth_token,
+        accessSecret: secretToken,
+    });
+    const result = await client.login(oauth_verifier);
+
+    await dbRefQuote.set({ token: result.accessToken, secretToken: result.accessSecret });
+});
+
+exports.tweetMeme = functions.region('australia-southeast1').runWith({ memory: "512MB", timeoutSeconds: 60 }).pubsub.schedule('every 60 minutes').onRun(async (context) => {
+    const dbSnapshot = await dbRefMeme.get();
+    const { secretToken, token } = dbSnapshot.data();
+
+    const client = new TwitterApi({
+        appKey: config.memeConfig.appKey,
+        appSecret: config.memeConfig.appSecret,
+        accessToken: token,
+        accessSecret: secretToken,
+    });
+    
+    const clt = await client.currentUser();
+
+    let meme = await getMeme();
+
+    const response = await axios.get(meme.image, { responseType: 'arraybuffer' })
+    const buffer = Buffer.from(response.data, "utf-8")
+
+    const mediaId = await client.v1.uploadMedia(buffer, { mimeType: 'image/png' })
+    console.log(mediaId)
+    const { data } = await client.v1.tweet(
+        `${meme.title}`, { media_ids: [mediaId] }
+    );
+
+    //response.send(data);
+});
+
+
+exports.tweetQuote = functions.region('australia-southeast1').pubsub.schedule('every 60 minutes').onRun(async (context) => {
+    const dbSnapshot = await dbRefQuote.get();
+    const { secretToken, token } = dbSnapshot.data();
+
+    const client = new TwitterApi({
+        appKey: config.quoteConfig.appKey,
+        appSecret: config.quoteConfig.appSecret,
+        accessToken: token,
+        accessSecret: secretToken,
+    });
+
+    const clt = await client.currentUser();
+
+    let quote = "";
+    let author = "";
+    let hashtags = "";
+    axios.get('https://api.quotable.io/random').then(async(e) => {
+
+    
+    e.data.tags.forEach(element => {
+        hashtags += `#${element} `
+    });
+
+    quote = e.data.content;
+    author = e.data.author;
+    const { data } = await client.v2.tweet(
+        `"${quote}"\n-${author}\n\n#quote #inspiration ${hashtags}`
+     );
+}).catch((e) => { console.log(e); })
+
+    
+
+    //response.send(data);
+});
---
+++ functions/scrape.js
@@ -0,0 +1,90 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const puppeteer = require('puppeteer');
+
+
+const getMeme = async () => {
+    let returnData = {title: "", image: "", link: ""}
+    const browser = await puppeteer.launch({
+      headless: true,
+      timeout: 20000,
+      ignoreHTTPSErrors: true,
+      slowMo: 0,
+      args: [
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-setuid-sandbox',
+        '--no-first-run',
+        '--no-sandbox',
+        '--no-zygote',
+        '--window-size=1280,720',
+      ],
+    });
+  
+    try {
+      const page = await browser.newPage();
+  
+      await page.setViewport({ width: 1280, height: 720 });
+  
+      // Block images, videos, fonts from downloading
+      await page.setRequestInterception(true);
+  
+      page.on('request', (interceptedRequest) => {
+        const blockResources = ['script', 'stylesheet', 'image', 'media', 'font'];
+        if (blockResources.includes(interceptedRequest.resourceType())) {
+          interceptedRequest.abort();
+        } else {
+          interceptedRequest.continue();
+        }
+      });
+  
+      // Change the user agent of the scraper
+      await page.setUserAgent(
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
+      );
+
+      const subReddits = ['memes', 'MemeEconomy', 'wholesomememes', 'dankmemes', 'ProgrammerHumor', 'dogmemes', 'AnimalMemes', 'funny']
+      const categories = ['hot', 'new', 'top/?t=hour', 'top/?t=day', 'top/?t=week', 'top/?t=month']
+  
+      await page.goto(`https://www.reddit.com/r/${subReddits[Math.floor(Math.random() * subReddits.length)]}/${categories[Math.floor(Math.random() * categories.length)]}`, {
+        waitUntil: 'domcontentloaded',
+      });
+  
+      const storySelector = '.Post';
+  
+      // Only get the top 10 headlines
+      let divs = await page.$$eval(storySelector, (e) => e.map((n) => n.innerHTML));
+
+      let index = Math.floor(Math.random() * divs.length)
+
+      let dom = new JSDOM(divs[index]);
+      let title = dom.window.document.querySelector('._eYtD2XCVieq6emjKBH3m');
+      let image = dom.window.document.querySelector('.ImageBox-image');
+      let url = dom.window.document.querySelector('a._2INHSNB8V5eaWp4P0rY_mE');
+
+     
+
+      while(!title || !title.innerHTML || !image || !image.src || !url || !url.href) {
+        index = Math.floor(Math.random() * divs.length)
+        dom = new JSDOM(divs[index]);
+        title = dom.window.document.querySelector('._eYtD2XCVieq6emjKBH3m');
+      image = dom.window.document.querySelector('.ImageBox-image');
+      url = dom.window.document.querySelector('a._2INHSNB8V5eaWp4P0rY_mE');
+      }
+
+      returnData.image = image.src;
+      returnData.title = title.innerHTML + " #memes #memesdaily";
+      returnData.link = 'https://www.reddit.com' + url.href;
+      
+    } catch (error) {
+      console.log(error);
+    } finally {
+      if (browser) {
+        await browser.close();
+      }
+    }
+    
+    return returnData;
+  };
+  
+module.exports = getMeme;
